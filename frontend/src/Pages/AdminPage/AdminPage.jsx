e5888f10e1f8b78bb0cbe1e308427630cbe89ebc
Minh Tran <minhtran.2404@outlook.com>
2022-11-22 10:59:54 +0700

    Add authorization fe

--- frontend/src/Pages/AdminPage/AdminPage.jsx
+++ frontend/src/Pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Contexts/AuthContext";
+import { STAFF } from "../../Constants/SystemConstants";
 
 export function AdminPage() {
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const role = authContext.userRole;
+
+    if (role === null || role === "") {
+      return navigate("/login");
+    }
+  
+    if (role === STAFF) {
+      return navigate("/");
+    }
+  })
+
   return (
     <>
       <Outlet />
