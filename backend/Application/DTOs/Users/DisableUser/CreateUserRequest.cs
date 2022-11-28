09c766f5aa36c51d08c67f423847734020df4911
Lê Thành Đạt <datdatle2002@gmail.com>
2022-11-28 14:18:20 +0700

    fix create
    fix using repo

---
+++ backend/Application/DTOs/Users/DisableUser/CreateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Users.CreateUser;
+
+public class DisableUserRequest
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    public string LastName { get; set; } = null!;
+}
