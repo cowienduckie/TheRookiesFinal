e4ccf246807caa5f76ce2cc310d37ace0c45b331
hai18822 <74532483+hai18822@users.noreply.github.com>
2022-11-30 17:01:19 +0700

    update disable user page

--- frontend/src/Pages/AdminPage/ManageUserPage/DisableUserPage.jsx
+++ frontend/src/Pages/AdminPage/ManageUserPage/DisableUserPage.jsx
@@ -1,49 +1,66 @@
 import { Button, Divider, Modal, Space } from "antd";
-import { CloseOutlined } from "@ant-design/icons";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function DisableUserPage() {
   let { id } = useParams();
+  const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(true);
-console.log(id);
+  const assigment = true;
+  const onCancel = () => {
+    navigate(-1);
+  };
   return (
     <>
-      {/* <Modal open={isModalOpen} closable={false} footer={false}>
-        <div className=" flex justify-between content-center">
-          <h1 className="text-2xl text-red-600 font-bold">
-            Can not disable user
-          </h1>
-          <button
-            className="border-2 border-red-600 text-red-600 pb-1 pl-1 pr-1 rounded h-fit"
-            onClick={() => {
-              setIsModalOpen(false);
-            }}
-          >
-            <CloseOutlined />
-          </button>
-        </div>
-        <Divider />
-        <div className="pl-12 pb-5">
-          <p>There are valid assignments belonging to this user.</p>
-          <p>Please close all assignments before disabling user.</p>
-        </div>
-      </Modal> */}
-      <Modal open={isModalOpen} closable={false} footer={false}>
-        <div className="pl-12 flex justify-between content-center">
-          <h1 className="text-2xl text-red-600 font-bold">
-            Are you sure?
-          </h1>          
-        </div>
-        <Divider />
-        <div className="pl-12 pb-5">
-          <p className="mb-5">Do you want to disable this user?</p>
-          <Space>
-            <Button type="primary" danger onClick={()=>{setIsModalOpen(false)}}>Disable</Button>
-            <Button onClick={()=>{setIsModalOpen(false)}}>Cancel</Button>
-          </Space>
-        </div>
-      </Modal>
+      {assigment ? (
+        <Modal
+          open={isModalOpen}
+          closable={true}
+          footer={false}
+          onCancel={onCancel}
+        >
+          <div className=" flex content-center justify-between">
+            <h1 className="text-2xl font-bold text-red-600">
+              Can not disable user
+            </h1>
+          </div>
+          <Divider />
+          <div className="pl-12 pb-5">
+            <p>There are valid assignments belonging to this user.</p>
+            <p>Please close all assignments before disabling user.</p>
+          </div>
+        </Modal>
+      ) : (
+        <Modal open={isModalOpen} closable={false} footer={false}>
+          <div className="flex content-center justify-between pl-12">
+            <h1 className="text-2xl font-bold text-red-600">Are you sure?</h1>
+          </div>
+          <Divider />
+          <div className="pl-12 pb-5">
+            <p className="mb-5">Do you want to disable this user?</p>
+            <Space>
+              <Button
+                type="primary"
+                danger
+                onClick={() => {
+                  setIsModalOpen(false);
+                  navigate("/admin/manage-user");
+                }}
+              >
+                Disable
+              </Button>
+              <Button
+                onClick={() => {
+                  setIsModalOpen(false);
+                  navigate("/admin/manage-user");
+                }}
+              >
+                Cancel
+              </Button>
+            </Space>
+          </div>
+        </Modal>
+      )}
     </>
   );
 }
