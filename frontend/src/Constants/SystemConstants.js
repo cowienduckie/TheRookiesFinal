3448fcf48241150e86cf38f530abb5cc823a8b1a
Minh Tran <minhtran.2404@outlook.com>
2022-12-01 00:05:01 +0700

    Integrate API

--- frontend/src/Constants/SystemConstants.js
+++ frontend/src/Constants/SystemConstants.js
@@ -1,5 +1,4 @@
-//export const API_BASE_URL = "https://group4b6msapi.azurewebsites.net";
-export const API_BASE_URL = "https://localhost:7162";
+export const API_BASE_URL = "https://group4b6msapi.azurewebsites.net";
 export const TOKEN_KEY = "jwt-token";
 export const ROLE_KEY = "user-role";
 export const USERNAME_KEY = "username-key";
